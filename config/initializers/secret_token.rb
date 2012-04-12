3b2786d3c5a30a673b52519bedbcc34d38c42005
Martin Volerich <martin@volerich.com>
2012-04-12 09:03:50 -0400

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Vague::Application.config.secret_token = 'df059c37da984aac71e0fdb3b4c5788e07338281710767aa00e94dec1e61a2bc28006286c7e0a231312d43f3f0b033f2fe814240d893df0591090524ed2698e6'
